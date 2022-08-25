bcd429d3be66db6f31c37fb71adb4cc80bc5aab4
FabioFrAzevedo <81392084+FabioFrAzevedo@users.noreply.github.com>
2022-08-25 17:05:01 -0300

    Add files via upload

---
+++ SomarDoisNumero.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class SomarDoisNumero {
+  public static Scanner sc = new Scanner(System.in);
+  
+  public static void main(String aegs[]) {
+	   int num1,num2,soma;
+	   
+	   System.out.println("Digite um número");
+	   num1 = sc.nextInt();
+	   System.out.println("Digite outro número");
+	   num2 = sc.nextInt();
+	   
+	   soma = num1 + num2;
+	   System.out.println("Soma: "+soma);
+  }
+
+}
+   
